dd6f2efac38440b44cee00f3989d3de8d7d97629
avadlapatla <avadlapatla>
2023-03-31 23:21:36 -0700

    Re-written using Textual libraries

--- aws-ec2-instance-manager.py
+++ aws-ec2-instance-manager.py
@@ -4,9 +4,10 @@ import os
 import subprocess
 import shlex
 import socket
-from prompt_toolkit import PromptSession
-from prompt_toolkit.completion import WordCompleter
-from prompt_toolkit.shortcuts import CompleteStyle
+from textual.app import App
+from textual.widgets import Header, SearchToolbar, Footer, ListBox
+from textual import events
+from rich.text import Text
 
 def get_ec2_instances():
     client = boto3.client('ec2')
@@ -33,15 +34,36 @@ def find_available_port():
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
 
-def main():
-    instances = get_ec2_instances()
-    instance_ids = [instance['InstanceId'] for instance in instances]
-    completer = WordCompleter(instance_ids, ignore_case=True, sentence=True)
-    session = PromptSession(completer=completer, complete_style=CompleteStyle.READLINE_LIKE)
+class InstanceListBox(ListBox):
+    def __init__(self, instances):
+        self.instances = instances
+        self.filtered_instances = instances
+        super().__init__(self.get_instance_names())
 
-    while True:
-        instance_id = session.prompt("Enter the Instance ID (partial or complete) and press Enter to connect: ")
+    def get_instance_names(self):
+        return [Text(instance["InstanceId"]) for instance in self.filtered_instances]
 
+    def filter_instances(self, search_term: str):
+        self.filtered_instances = [
+            i for i in self.instances if search_term.lower() in i["InstanceId"].lower()
+        ]
+        self.update(self.get_instance_names())
+
+class EC2InstanceManager(App):
+    async def on_mount(self) -> None:
+        instances = get_ec2_instances()
+        self.instance_list = InstanceListBox(instances)
+        await self.view.dock(Header("EC2 Instance Manager"), edge="top")
+        await self.view.dock(SearchToolbar(name="search"), edge="top")
+        await self.view.dock(Footer("Press ESC to exit"), edge="bottom")
+        await self.view.dock(self.instance_list, edge="left", size=40)
+
+    async def on_text_changed(self, event: events.TextChanged) -> None:
+        if event.sender.name == "search":
+            self.instance_list.filter_instances(event.text)
+
+    async def action_select_instance(self):
+        instance_id = self.instance_list.choice
         if instance_id:
             local_port = find_available_port()
             print(f"Starting port forwarding session on local port {local_port}...")
@@ -49,5 +71,14 @@ def main():
         else:
             print("No instance selected.")
 
+    async def on_key(self, event: events.Key) -> None:
+        if event.key == "enter":
+            await self.action_select_instance()
+        elif event.key == "escape":
+            await self.quit()
+        else:
+            await super().on_key(event)
+
 if __name__ == "__main__":
-    main()
+    app = EC2InstanceManager()
+    app.run()
